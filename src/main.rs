dca667f29fa38701591c8a29794a15874d05df38
Daniel Csillag <dccsillag@gmail.com>
2021-08-17 19:23:43 -0300

    Add options for controlling row and column to draw image at

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,31 @@
-use std::path::PathBuf;
-use anyhow::{Result, Context};
-use structopt::StructOpt;
+use anyhow::{Context, Result};
 use image::io::Reader as ImageReader;
 use image::RgbImage;
+use std::path::PathBuf;
 use std::process::Command;
+use structopt::StructOpt;
 use x11rb::{
     connection::Connection,
+    image as x11image,
     protocol::xproto::{
-        Window,
-        ConnectionExt,
+        ConnectionExt, CreateGCAux, CreateWindowAux, Screen, VisualClass, Visualid, Window,
         WindowClass,
-        CreateWindowAux,
-        CreateGCAux,
-        Screen,
-        Visualid,
-        VisualClass,
     },
-    image as x11image
 };
-
 
 #[derive(StructOpt)]
 struct Opt {
     /// Path to image file
     #[structopt(parse(from_os_str))]
     image_file: PathBuf,
+
+    /// Terminal row to display the image in
+    #[structopt()]
+    row: i16,
+
+    /// Terminal column to display the image in
+    #[structopt()]
+    col: i16,
 }
 
 /// Taken from https://github.com/psychon/x11rb/blob/84a877d72b87ac4de82aa77c4cfc0598ed41732a/examples/display_ppm.rs#L73-L107
@@ -71,13 +72,42 @@ fn get_current_window_id() -> Result<Window> {
         .with_context(|| "Failed to run xdotool")?;
     let stdout = String::from_utf8_lossy(&output.stdout);
     // TODO check exit code
-    stdout.trim().parse().with_context(|| "Couldn't parse window ID number from xdotool")
+    stdout
+        .trim()
+        .parse()
+        .with_context(|| "Couldn't parse window ID number from xdotool")
 }
 
-fn show_image(image: RgbImage, window: Window) -> Result<()> {
+fn rowcol_to_pixels(
+    conn: &impl Connection,
+    window: Window,
+    (row, col): (i16, i16),
+) -> Result<(i16, i16)> {
+    // Get geometry of the given window
+    let window_geometry = conn.get_geometry(window)?.reply()?;
+    dbg!(window_geometry);
+
+    let (cols, rows) = termion::terminal_size().with_context(|| "Could not get terminal size")?;
+    let (xpixels, ypixels) =
+        termion::terminal_size_pixels().with_context(|| "Could not get terminal size in pixels")?;
+    let pixels_per_row = (ypixels / rows) as i16;
+    let pixels_per_col = (xpixels / cols) as i16;
+    let yoffset = ((window_geometry.height - ypixels) as i16) / 2;
+    let xoffset = ((window_geometry.width - xpixels) as i16) / 2;
+
+    Ok((
+        xoffset + col * pixels_per_col,
+        yoffset + row * pixels_per_row,
+    ))
+}
+
+fn show_image(image: RgbImage, window: Window, (x, y): (i16, i16)) -> Result<()> {
     // Connect to the X server
     let (conn, screen_num) = x11rb::connect(None).with_context(|| "Couldn't connect to X")?;
     let screen = &conn.setup().roots[screen_num];
+
+    // Convert (x, y) to pixels
+    let (x, y) = rowcol_to_pixels(&conn, window, (x, y))?;
 
     // Get image information and create x11rb image
     let (w, h) = image.dimensions();
@@ -107,17 +137,11 @@ fn show_image(image: RgbImage, window: Window) -> Result<()> {
     conn.create_gc(
         gc_id,
         screen.root,
-        &CreateGCAux::new().graphics_exposures(0)
+        &CreateGCAux::new().graphics_exposures(0),
     )?;
     // Create and paint pixmap
     let pixmap_id = conn.generate_id()?;
-    conn.create_pixmap(
-        screen.root_depth,
-        pixmap_id,
-        screen.root,
-        w,
-        h,
-    )?;
+    conn.create_pixmap(screen.root_depth, pixmap_id, screen.root, w, h)?;
     img.put(&conn, pixmap_id, gc_id, 0, 0)?;
     // Create window
     let win_id = conn.generate_id()?;
@@ -134,7 +158,7 @@ fn show_image(image: RgbImage, window: Window) -> Result<()> {
         0,
         &CreateWindowAux::default().background_pixmap(pixmap_id),
     )?;
-    conn.reparent_window(win_id, window, 0, 0)?;
+    conn.reparent_window(win_id, window, x, y)?;
 
     // Free pixmap&gcontext
     conn.free_pixmap(pixmap_id)?;
@@ -155,13 +179,11 @@ fn show_image(image: RgbImage, window: Window) -> Result<()> {
 fn main() -> Result<()> {
     let opt: Opt = Opt::from_args();
 
-    let image: RgbImage = ImageReader::open(opt.image_file)?
-        .decode()?
-        .to_rgb8();
+    let image: RgbImage = ImageReader::open(opt.image_file)?.decode()?.to_rgb8();
 
     let window = get_current_window_id()?;
 
-    show_image(image, window)?;
+    show_image(image, window, (opt.row, opt.col))?;
 
     Ok(())
 }
